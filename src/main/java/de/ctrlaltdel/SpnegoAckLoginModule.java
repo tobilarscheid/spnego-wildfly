ce78eb8d1abc51853dd5b13b1b77464857333777
tobilarscheid <tobilarscheid@gmail.com>
2016-05-06 21:12:25 +0200

    remove spnego specifics

--- src/main/java/de/ctrlaltdel/SpnegoAckLoginModule.java
+++ src/main/java/de/ctrlaltdel/SpnegoAckLoginModule.java
@@ -1,83 +1,53 @@
 package de.ctrlaltdel;
 
-import de.ctrlaltdel.SpnegoAuthenticationMechanism.SimplePrincipal;
-
-import javax.security.auth.Subject;
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.CallbackHandler;
-import javax.security.auth.callback.NameCallback;
-import javax.security.auth.callback.PasswordCallback;
-import javax.security.auth.login.LoginException;
-import javax.security.auth.spi.LoginModule;
-
 import java.security.Principal;
 import java.util.Map;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
 
 /**
  * SpnegoAckLoginModule
  */
 public class SpnegoAckLoginModule implements LoginModule {
 
-    private Subject subject;
-    private Principal principal;
-    private Map sharedState;
-    private CallbackHandler callbackHandler;
+	private Subject subject;
+	private Principal principal;
+	private Map sharedState;
+	private CallbackHandler callbackHandler;
 
-    @Override
-    public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState, Map<String, ?> options) {
-        this.subject = subject;
-        this.sharedState = sharedState;
-        this.callbackHandler = callbackHandler;
-    }
+	@Override
+	public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState,
+			Map<String, ?> options) {
+		this.subject = subject;
+		this.sharedState = sharedState;
+		this.callbackHandler = callbackHandler;
+	}
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public boolean login() throws LoginException {
+	@Override
+	@SuppressWarnings("unchecked")
+	public boolean login() throws LoginException {
 
-        NameCallback nc = new NameCallback("name");
-        PasswordCallback pc = new PasswordCallback("password", false);
-        try {
-            callbackHandler.handle(new Callback[]{nc, pc});
-        } catch (Exception x) {
-            throw new LoginException(x.getMessage());
-        }
+		System.out.println("login called");
+		return true;
+	}
 
-        String name = nc.getName();
-        char[] passwordChar = pc.getPassword();
-        String credential = passwordChar != null ? new String(passwordChar) : null;
+	@Override
+	public boolean commit() throws LoginException {
+		System.out.println("commit called");
+		return true;
+	}
 
-        long loginTime = Long.valueOf(credential);
-        if (loginTime < SpnegoAuthenticationMechanism.UPTIME) {
-            return false;
-        }
+	@Override
+	public boolean abort() throws LoginException {
+		return false;
+	}
 
-        SimplePrincipal simplePrincipal = new SimplePrincipal(name, credential);
-
-        sharedState.put("javax.security.auth.login.name", simplePrincipal.getName());
-        sharedState.put("javax.security.auth.login.password", simplePrincipal.getCredential());
-
-        this.principal = simplePrincipal;
-        return true;
-    }
-
-    @Override
-    public boolean commit() throws LoginException {
-        if (principal == null) {
-            return false;
-        }
-        subject.getPrincipals().add(principal);
-        return true;
-    }
-
-    @Override
-    public boolean abort() throws LoginException {
-        return false;
-    }
-
-    @Override
-    public boolean logout() throws LoginException {
-        return false;
-    }
-
+	@Override
+	public boolean logout() throws LoginException {
+		return false;
+	}
 
 }
--- src/main/java/de/ctrlaltdel/SpnegoAuthenticationMechanism.java
+++ src/main/java/de/ctrlaltdel/SpnegoAuthenticationMechanism.java
@@ -1,123 +1,83 @@
 package de.ctrlaltdel;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.wildfly.extension.undertow.security.AccountImpl;
+import java.security.Principal;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.security.Principal;
-import java.util.Collections;
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.undertow.security.api.AuthenticationMechanism;
 import io.undertow.security.api.AuthenticationMechanismFactory;
 import io.undertow.security.api.SecurityContext;
-import io.undertow.security.idm.Account;
-import io.undertow.security.idm.IdentityManager;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.form.FormParserFactory;
-import io.undertow.servlet.handlers.ServletRequestContext;
-import net.sourceforge.spnego.SpnegoAuthenticator;
-import net.sourceforge.spnego.SpnegoPrincipal;
 
 /**
  * SpnegoHandler
  */
 public class SpnegoAuthenticationMechanism implements AuthenticationMechanism {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SpnegoAuthenticationMechanism.class);
+	private static final Logger LOG = LoggerFactory.getLogger(SpnegoAuthenticationMechanism.class);
 
-    private final SpnegoAuthenticator authenticator;
-    private final String mechanismName;
+	private final String mechanismName;
 
-    static final long UPTIME = System.currentTimeMillis();
+	static final long UPTIME = System.currentTimeMillis();
 
-    private SpnegoAuthenticationMechanism(String mechanismName) {
-        try {
-            this.authenticator = new SpnegoAuthenticator();
-            LOG.info("Spnego-Auth started");
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        }
-        this.mechanismName = mechanismName;
+	private SpnegoAuthenticationMechanism(String mechanismName) {
+		this.mechanismName = mechanismName;
+		System.out.println("authentication mechanism constructor called");
 
-    }
+	}
 
+	@Override
+	public AuthenticationMechanismOutcome authenticate(HttpServerExchange exchange, SecurityContext securityContext) {
 
-    @Override
-    public AuthenticationMechanismOutcome authenticate(HttpServerExchange exchange, SecurityContext securityContext) {
+		System.out.println("authenticate called");
 
-        ServletRequestContext servletRequestContext = exchange.getAttachment(ServletRequestContext.ATTACHMENT_KEY);
-        HttpServletRequest request = servletRequestContext.getOriginalRequest();
-        HttpServletResponse response = servletRequestContext.getOriginalResponse();
+		return AuthenticationMechanismOutcome.AUTHENTICATED;
 
-        String accountName;
-        try {
-            SpnegoPrincipal principal = this.authenticator.authenticate(request, response);
-            accountName = principal == null ? null : principal.getName();
-        } catch (Exception gsse) {
-            LOG.error("HTTP Authorization Header=" + request.getHeader("Authorization"));
-            return AuthenticationMechanismOutcome.NOT_ATTEMPTED;
-        }
+	}
 
-        if (accountName == null) {
-            return AuthenticationMechanismOutcome.NOT_AUTHENTICATED;
-        }
+	@Override
+	public ChallengeResult sendChallenge(HttpServerExchange exchange, SecurityContext securityContext) {
+		System.out.println("send challenge called");
+		return new ChallengeResult(true, HttpServletResponse.SC_UNAUTHORIZED);
+	}
 
-        SimplePrincipal principal = new SimplePrincipal(accountName, String.valueOf(System.currentTimeMillis()));
+	public static final class Factory implements AuthenticationMechanismFactory {
+		@Override
+		public AuthenticationMechanism create(String mechanismName, FormParserFactory formParserFactory,
+				Map<String, String> properties) {
+			System.out.println("factpry called");
+			return new SpnegoAuthenticationMechanism(mechanismName);
+		}
+	}
 
-        IdentityManager identityManager = securityContext.getIdentityManager();
-        Account account = identityManager.verify(new AccountImpl(principal, Collections.<String>emptySet(), principal.getCredential()));
-        if (account == null) {
-            return AuthenticationMechanismOutcome.NOT_AUTHENTICATED;
-        }
+	public static class SimplePrincipal implements Principal {
+		private final String name;
+		private final String credential;
 
-        securityContext.authenticationComplete(account, mechanismName, true);
+		public SimplePrincipal(String name, String credential) {
+			this.credential = credential;
+			int idx = name.indexOf('@');
+			this.name = 0 < idx ? name.substring(0, idx) : name;
+		}
 
-        LOG.debug("authentificated {}", principal);
+		@Override
+		public String getName() {
+			return name;
+		}
 
-        return AuthenticationMechanismOutcome.AUTHENTICATED;
+		public String getCredential() {
+			return credential;
+		}
 
-    }
-
-
-    @Override
-    public ChallengeResult sendChallenge(HttpServerExchange exchange, SecurityContext securityContext) {
-        return new ChallengeResult(true, HttpServletResponse.SC_UNAUTHORIZED);
-    }
-
-    public static final class Factory implements AuthenticationMechanismFactory {
-        @Override
-        public AuthenticationMechanism create(String mechanismName, FormParserFactory formParserFactory, Map<String, String> properties) {
-            return new SpnegoAuthenticationMechanism(mechanismName);
-        }
-    }
-
-    public static class SimplePrincipal implements Principal {
-        private final String name;
-        private final String credential;
-        public SimplePrincipal(String name, String credential) {
-            this.credential = credential;
-            int idx = name.indexOf('@');
-            this.name = 0 < idx ? name.substring(0, idx) : name;
-        }
-
-        @Override
-        public String getName() {
-            return name;
-        }
-
-        public String getCredential() {
-            return credential;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
-    }
-
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
 
 }
